6df8738b2bc8e77befc6bba1fee379aeff5365b9
Nishit Kamdar <kamdarnishit@gmail.com>
2022-04-01 20:51:47 +0530

    Stashing

---
+++ packages/integration/src/index.ts
@@ -0,0 +1,7 @@
+import { myCommons } from "@sindric-lib-ui/commons";
+import { myComponent } from "@sindric-lib-ui/components";
+import { createDropUrl } from "@sindric-lib-ui/endpoints";
+
+console.log(myCommons());
+console.log(myComponent());
+console.log(createDropUrl);
