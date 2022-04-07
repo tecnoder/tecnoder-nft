10202dfbd8c1439441bb864db8e173d93c379595
Nishit Kamdar <kamdarnishit@gmail.com>
2022-04-07 14:57:18 +0530

    Updated VerifyTotp

--- packages/components/src/verify-totp/index.tsx
+++ packages/components/src/verify-totp/index.tsx
@@ -1,6 +1,7 @@
 import { postData } from '@sindric-lib-ui/commons/lib/api/api';
 import { validateTotpUrl } from '@sindric-lib-ui/endpoints';
 import React, { useState } from 'react';
+import { Button } from '../button';
 import InputField from '../InputField';
 import Message from '../Message';
 
@@ -61,62 +62,18 @@ export const VerifyTotp: React.FC<{[key:string]: any}> = ({onSuccess, onError}) 
             setIsVerifying(false);
             setButtonText(default_button_text);
         }
-//        }
-        /*else{
-            const response  = await ApiService.post(changeTwoFactorAuthenticationUrl, {
-                "isTwoFactor": type,
-                "totpCode": verifyCode
-            })
-            if(response.status === "SUCCESS"){
-                setIsVerifying(false);
-                setIsVerified(true);
-                setButtonText(default_button_text);
-                if(sendAcknowledge){
-                    setIsVerifiedState(true);
-                }
-                else{
-                    const headers:any = {};
-                    headers['OWASP_CSRFTOKEN'] = response.data.jwtId;
-                    const logoutResponse = await ApiService.post(logoutUrl, null, {headers:headers})
-                    if(logoutResponse){
-                        dispatch(getUserSession()).then(()=>{
-                            history.push('/req/status', {
-                                data: {
-                                message1: `You have successfully ${type === "N" ? 'disabled' : 'enabled'} Two factor authentication`,
-                                btnText1: "Login",
-                                btnUrl1: "/login"
-                                }
-                            })
-                        });  
-                    }
-                }
-            }
-            else{
-                setError(response.errorDesc)
-                setIsVerifying(false);
-                setButtonText(default_button_text);
-            }
-        }*/
     }
 
   return (
     <>
     <div className='mb-1 normal-fs'>Enter Two Factor Authentication code</div>
     {error.length > 0 && <div className="mb-2"><Message type="error" message={error}/></div>}
-    <form className={`${`verify_code_form`} w-100 verifyCodeWrapper`}>
+    <form className={`${`verify_code_form`} w-100 verifyCodeWrapper`} onSubmit={verifyTotp}>
         <div className={`verify_code_field`}>
             <InputField wrapperClass="mb-1" type="text" name="verifyCode" placeholder="Two factor authentication code" value={verifyCode} errors={errors} onChange={(e:any)=>setVerifyCode(e.target.value)}/>
         </div>
-        <div className={`verify_code_button`}>
-        <button
-            type="submit"
-            disabled={isVerifying||!verifyCode||verifyCode.trim().length===0}
-            className={`btn btn-sm btn-black`}
-            onClick={(ev) => verifyTotp(ev)}
-        >
-            {buttonText}
-        </button>
-            {/* <Button type="primary" aria-disabled disabled={isVerifying||!verifyCode||verifyCode.trim().length===0}>{buttonText}</Button> */}
+        <div className={`verify_code_button`} onClick={(ev) => verifyTotp(ev)}>
+            <Button type="Primary" disabled={isVerifying||!verifyCode||verifyCode.trim().length===0}></Button>
         </div>
     </form>
     </>
